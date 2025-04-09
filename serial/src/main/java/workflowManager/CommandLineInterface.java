512bd55307a24fe3c07c8023a7bbabe5a3166690
KwtsPls <constantinepls99@gmail.com>
2025-04-09 18:37:23 +0300

    Added workflow for Gia.nt and csv files to run without user interface

--- serial/src/main/java/workflowManager/CommandLineInterface.java
+++ serial/src/main/java/workflowManager/CommandLineInterface.java
@@ -12,49 +12,62 @@ import static workflowManager.CommandLineInterfaceUtils.*;
 public class CommandLineInterface {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to JedAI-spatial's CLI");
+        if (args[0].equals("custom")) {
+            String sourceFilePath = args[1];
+            AbstractReader sourceReader = DataType.getReader(DataType.CSV,sourceFilePath);
 
-        int exit = 2;
-        while (exit == 2) {
+            String targetFilePath = args[2];
+            AbstractReader targetReader = DataType.getReader(DataType.CSV, targetFilePath);
 
-            String sourceFilePath = readPath("Please provide the path of your source dataset:");
-            int sourceDataType = readInt("Please choose the data types of your source data:", DATA_TYPE);
-            AbstractReader sourceReader = DataType.getReader(DataType.values()[sourceDataType-1], sourceFilePath);
+            String outputFile = args[3];
 
-            String targetFilePath = readPath("Please provide the path of your target dataset:");
-            int targetDataType = readInt("Please choose the data types of your source data:", DATA_TYPE);
-            AbstractReader targetReader = DataType.getReader(DataType.values()[targetDataType-1], targetFilePath);
+            ProgressiveAlgorithms.runAlgorithm(ProgressiveAlgorithms.GIANT, sourceReader, targetReader, outputFile);
 
-            int toExport = readInt("Please choose whether you want to export your results:", YES_OR_NO);
+        } else {
+            System.out.println("Welcome to JedAI-spatial's CLI");
 
-            String outputFile = null;
-            if (toExport == 1) outputFile = readPath("Please provide the path to your export file:");
+            int exit = 2;
+            while (exit == 2) {
 
-            int experimentType = readInt("Please choose one of the available Experiments:", EXPERIMENT_TYPE);
-            int algorithm;
-            switch (experimentType) {
-                case 1:
-                    algorithm = readInt("Please choose one of the available algorithms:", BATCH_ALGORITHMS);
-                    if (toExport == 1)
-                        BatchAlgorithms.runAlgorithm(BatchAlgorithms.values()[algorithm-1], sourceReader, targetReader, outputFile);
-                    else
-                        BatchAlgorithms.runAlgorithm(BatchAlgorithms.values()[algorithm-1], sourceReader, targetReader);
+                String sourceFilePath = readPath("Please provide the path of your source dataset:");
+                int sourceDataType = readInt("Please choose the data types of your source data:", DATA_TYPE);
+                AbstractReader sourceReader = DataType.getReader(DataType.values()[sourceDataType - 1], sourceFilePath);
 
-                    break;
+                String targetFilePath = readPath("Please provide the path of your target dataset:");
+                int targetDataType = readInt("Please choose the data types of your source data:", DATA_TYPE);
+                AbstractReader targetReader = DataType.getReader(DataType.values()[targetDataType - 1], targetFilePath);
 
-                case 2:
-                    algorithm = readInt("Please choose one of the available algorithms:", PROGRESSIVE_ALGORITHMS);
-                    if (toExport == 1)
-                        ProgressiveAlgorithms.runAlgorithm(ProgressiveAlgorithms.values()[algorithm-1], sourceReader, targetReader, outputFile);
-                    else
-                        ProgressiveAlgorithms.runAlgorithm(ProgressiveAlgorithms.values()[algorithm-1], sourceReader, targetReader);
+                int toExport = readInt("Please choose whether you want to export your results:", YES_OR_NO);
 
-                    break;
+                String outputFile = null;
+                if (toExport == 1) outputFile = readPath("Please provide the path to your export file:");
+
+                int experimentType = readInt("Please choose one of the available Experiments:", EXPERIMENT_TYPE);
+                int algorithm;
+                switch (experimentType) {
+                    case 1:
+                        algorithm = readInt("Please choose one of the available algorithms:", BATCH_ALGORITHMS);
+                        if (toExport == 1)
+                            BatchAlgorithms.runAlgorithm(BatchAlgorithms.values()[algorithm - 1], sourceReader, targetReader, outputFile);
+                        else
+                            BatchAlgorithms.runAlgorithm(BatchAlgorithms.values()[algorithm - 1], sourceReader, targetReader);
+
+                        break;
+
+                    case 2:
+                        algorithm = readInt("Please choose one of the available algorithms:", PROGRESSIVE_ALGORITHMS);
+                        if (toExport == 1)
+                            ProgressiveAlgorithms.runAlgorithm(ProgressiveAlgorithms.values()[algorithm - 1], sourceReader, targetReader, outputFile);
+                        else
+                            ProgressiveAlgorithms.runAlgorithm(ProgressiveAlgorithms.values()[algorithm - 1], sourceReader, targetReader);
+
+                        break;
+                }
+
+                exit = readInt("Please choose Yes to exit the program or No to perform another experiment:", YES_OR_NO);
             }
 
-            exit = readInt("Please choose Yes to exit the program or No to perform another experiment:", YES_OR_NO);
         }
-
     }
 
 }
